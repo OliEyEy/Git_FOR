2a70c14bb377e01997bc209affdcb8244187f29e
Ólafur Eysteinn Eysteinsson <olafurey298@nemi.tskoli.is>
2017-01-24 14:22:55 +0000

    Dæmi 3

--- FOR/git_VERKEFNI/Git.py
+++ FOR/git_VERKEFNI/Git.py
@@ -13,3 +13,24 @@ print("Liður 2")
 fornafn=input("Hvað er fornafn þitt? ")
 eftirnafn=input("Hvað er eftirnafn þitt? ")
 print("Halló",fornafn,eftirnafn)
+
+print(" ")
+#Læt notenda slá inn einhvern texta, segi honum hversu mikið af lástöfum, HÁSTÖFUM
+# og 'lástöfum á eftir HÁSTÖFUM' hann gerði.
+print("Liður 3")
+texti=input("Skrifaðu inn texta. ")
+lastafur=0
+hastafur=0
+laHastafur=0
+for staf in texti:
+    if staf.islower():
+        lastafur=lastafur+1
+    if staf.isupper():
+        hastafur=hastafur+1
+for x in range(len(texti)):
+    if texti[x].isupper():
+        if texti[x+1].islower():
+            laHastafur=laHastafur+1
+print("Það eru",lastafur,"lástafir.")
+print("Það eru",hastafur,"hástafir.")
+print("Það eru",laHastafur,"lástafir sem koma strax á eftir hástaf.")
