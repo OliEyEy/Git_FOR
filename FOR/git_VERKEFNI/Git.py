6cf3e2010c91e5b033eada417cab9ce7d50069ac
Ólafur Eysteinn Eysteinsson <olafurey298@nemi.tskoli.is>
2017-01-24 13:56:39 +0000

    Dæmi 1 !

---
+++ FOR/git_VERKEFNI/Git.py
@@ -0,0 +1,9 @@
+#Ólafur Eysteinn - 2001002980 - 24. Janúar 2017
+
+#Læt notenda slá inn tvær tölur, legg þær saman og margfalda þær
+print("Liður 1")
+tala1=int(input("Sláðu inn tölu 1. "))
+tala2=int(input("Sláðu inn tölu 2. "))
+print("Tölurnar lagðar saman eru",str(tala1+tala2))
+print("Tölurnar margfaldaðar saman eru",str(tala1*tala2))
+
